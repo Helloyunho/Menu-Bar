562410c30cd8e283080c215a1882742495fa4ee7
Helloyunho <yunho050840@gmail.com>
2022-01-18 16:09:59 +0900

    Initial Commit

---
+++ Menu Bar/Menu_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Menu_BarApp.swift
+//  Menu Bar
+//
+//  Created by Helloyunho on 2022/01/18.
+//
+
+import SwiftUI
+
+@main
+struct Menu_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
