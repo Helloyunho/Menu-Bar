45c93e39fb50dd1db34b616446a8cba7441f6088
Helloyunho <yunho050840@gmail.com>
2022-05-29 15:17:32 +0900

    ✨ feat: add require and exports
    and yeah i didn't wanna do this and wanted to use import keyword

--- Menu Bar/Classes/MenuItemInterpreter.swift
+++ Menu Bar/Classes/MenuItemInterpreter.swift
@@ -9,21 +9,51 @@ import AppKit
 import Foundation
 import JavaScriptCore
 
-class MenuItemInterpreter {
-    var jscontext: JSContext!
-    var scriptURL: URL
-    var scriptDirURL: URL
+class JSVMWithModuleCache: JSVirtualMachine {
+    var cachedModules = [String: JSValue?]()
+}
 
-    init(script: URL) {
-        let jscontext = JSContext()
+class MenuItemInterpreter {
+    let jscontext: JSContext!
+    let scriptURL: URL
+    let scriptDirURL: URL
+    let jsVM: JSVMWithModuleCache!
+
+    convenience init(script: URL) {
+        self.init(script: script, vm: JSVMWithModuleCache()!)
+    }
+    
+    init(script: URL, vm: JSVMWithModuleCache) {
+        let jscontext = JSContext(virtualMachine: vm)
         self.jscontext = jscontext!
+        self.jsVM = vm
+        
+        let requireBlock: @convention(block) (String) -> (JSValue?) = { module in
+            let expandedModule = NSString(string: module).expandingTildeInPath
+            if let cachedModule = vm.cachedModules[expandedModule] {
+                return cachedModule
+            }
+            
+            if !FileManager.default.fileExists(atPath: expandedModule) {
+                jscontext!.exception = JSValue(newErrorFromMessage: "Module \(expandedModule) not found.", in: jscontext)
+                return nil
+            }
+            
+            let context = MenuItemInterpreter(script: URL(fileURLWithPath: expandedModule), vm: vm)
+            let value = try! context.runScript()
+            context.jscontext.globalObject.objectForKeyedSubscript("exports")
+            vm.cachedModules[expandedModule] = value
+            return value
+        }
+
         self.jscontext.globalObject.setObject([:], forKeyedSubscript: "exports")
+        self.jscontext.globalObject.setObject(requireBlock, forKeyedSubscript: "require")
         self.scriptURL = script
         self.scriptDirURL = script.deletingLastPathComponent()
     }
 
-    func runScript(code: String) -> JSValue! {
-        return self.jscontext.evaluateScript(code)
+    func runScript(code: String, path: String) -> JSValue! {
+        return self.jscontext.evaluateScript(code, withSourceURL: URL(fileURLWithPath: path))
     }
     
     func runScript(path: String) throws -> JSValue! {
@@ -32,7 +62,7 @@ class MenuItemInterpreter {
         }
         
         let scriptContent = try String(contentsOfFile: path)
-        return self.runScript(code: scriptContent)
+        return self.runScript(code: scriptContent, path: path)
     }
     
     func runScript(url: URL) throws -> JSValue! {
